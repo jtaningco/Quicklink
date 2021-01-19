9a16000e88a410e180d12b5ad48560381cf5253c
Josh Taningco <64685600+jtaningco@users.noreply.github.com>
2021-01-19 23:13:16 +0800

    updated file structure

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\josh\documents\college\third year first sem\itmgt45_the-digital-economy\quicklink\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
