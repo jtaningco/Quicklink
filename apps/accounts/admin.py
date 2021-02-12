406d432cc29cc18d8d1b586c3a9dc9f0aefeedf3
Josh Taningco <josh.taningco@gmail.com>
2021-02-12 22:38:24 +0800

    update accounts admin view

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import *
+from .models import User, ShopInformation, CustomerInformation
 from django.contrib.auth import get_user_model
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(MerchantInformation)
+admin.site.register(ShopInformation)
 admin.site.register(CustomerInformation)
