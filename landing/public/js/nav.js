c257a8c7192b49b217623f7bd174444dc45aac5c
Josh Taningco <64685600+jtaningco@users.noreply.github.com>
2021-02-18 19:28:05 +0800

    early access navbar hotfix

--- landing/public/js/nav.js
+++ landing/public/js/nav.js
@@ -1,10 +1,11 @@
 window.onload = function() {
     // MOBILE NAVBAR
     var burger = document.getElementById("navBurger");
+    console.log(burger);
     
     burger.addEventListener("click", () => {
+        console.log("success");
         document.getElementById("navBurger").classList.toggle("open");
         document.getElementById("openMobileNav").classList.toggle("navbarActive");
-        document.getElementById("burgerLines").style.backgroundColor("white");
     });
 }
