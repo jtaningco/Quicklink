96085011a079877d1f6c4aade5c1e33424b5d957
Josh Taningco <64685600+jtaningco@users.noreply.github.com>
2021-02-18 23:14:22 +0800

    mobile nav padding fix

--- landing/public/js/main.js
+++ landing/public/js/main.js
@@ -8,8 +8,8 @@ window.onload = function() {
     });
 }
 
-// SCROLL EVENT
-function scroll() {
+// NAVBAR SCROLL
+window.onscroll = function() {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
         document.getElementById("navbar").classList.add("scrolled");
         document.getElementById("navLogo").classList.add("scrolled");
@@ -51,10 +51,48 @@ function scroll() {
     }
 }
 
-// NAVBAR SCROLL
-window.onscroll = scroll
 // NAVBAR SCROLL MOBILE
-window.ontouchmove = scroll
+window.ontouchmove = function() {
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+        document.getElementById("navbar").classList.add("scrolled");
+        document.getElementById("navLogo").classList.add("scrolled");
+
+        document.getElementById("navButton").classList.remove("primary-white-btn");
+        document.getElementById("navButton-link").classList.remove("primary-white-btn-link");
+
+        document.getElementById("navButton").classList.add("primary-btn");
+        document.getElementById("navButton-link").classList.add("primary-btn-link");
+
+        document.getElementById("navBurger").classList.add("scroll");
+        
+        var navOptions = document.getElementsByClassName("navOptions");
+        for(var i = 0, length=navOptions.length; i<length; i++) {
+            navOptions[i].classList.add("scrolled");
+        };
+    } else {
+        document.getElementById("navbar").classList.remove("scrolled");
+        document.getElementById("navLogo").classList.remove("scrolled");
+        
+        document.getElementById("navButton").classList.add("primary-white-btn");
+        document.getElementById("navButton-link").classList.add("primary-white-btn-link");
+
+        document.getElementById("navButton").classList.remove("primary-btn");
+        document.getElementById("navButton-link").classList.remove("primary-btn-link");
+
+        document.getElementById("navBurger").classList.remove("scroll");
+        
+        var navOptions = document.getElementsByClassName("navOptions");
+        for(var i = 0, length=navOptions.length; i<length; i++) {
+            // if window is greater than 1024px
+            if (window.matchMedia("(min-width: 1025px)").matches) {
+                // then make navoptions white text
+                navOptions[i].classList.remove("scrolled");
+            } else {
+                navOptions[i].classList.add("scrolled");
+            }
+        };
+    }
+}
 
 // ON RESIZE GET NAV OPTIONS BACK TO WHITE
 window.onresize = function() {
